93e9e13b51a8f6ff7ab8d8de76a78e3f03e5534a
Dustin Battles <dbattles2001@knights.ucf.edu>
2021-10-01 14:12:57 -0400

    Exercise 30 finito

--- exercise30/src/main/java/baseline/solution30.java
+++ exercise30/src/main/java/baseline/solution30.java
@@ -19,7 +19,13 @@ public class solution30 {
 
         for(int i = 1; i<13; i++){
             for(int j = 1; j<13; j++){
-                System.out.printf("%d ", i*j);
+                if(i*j < 10){
+                    System.out.printf(" ");
+                }
+                if(i*j < 100){
+                    System.out.printf(" ");
+                }
+                System.out.printf("%d  ", i*j);
             }
             System.out.printf("\n");
         }
